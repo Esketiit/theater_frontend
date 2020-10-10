9dd2d87056241fc371186ade9b0872d0ab7e8931
github username <heftygac@gmail.com>
2020-10-09 20:13:38 -0400

    added user registration to /home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,39 @@ import { withRouter } from 'react-router-dom';
 
 class Home extends React.Component {
     state = {
-        name: ""
+        name: "",
+        password: "",
+        password_confirmation: "",
+        errors: ""
     }
 
     handleChange = e => {
-        this.setState({ [e.target.name]: e.target.value })
         console.log(this.state)
+        this.setState({ [e.target.name]: e.target.value })
+        // console.log(this.state)
     }
 
-    signIn = () => {
-        this.props.login(this.state.name)
-        this.props.history.push('/select');
+    onSubmit = (e) => {
+        e.preventDefault()
+        fetch("http://localhost:3000/registrations", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                user: {
+                    username: this.state.name,
+                    password: this.state.password,
+                    password_confirmation: this.state.password_confirmation
+                }
+            })
+        },
+            { withCredentials: true })
+            .then(resp => console.log(resp, "registration"))
+            .catch(error => console.log(error, "error"))
+
+        console.log(this.state)
     }
 
     componentDidMount() {
@@ -22,7 +44,6 @@ class Home extends React.Component {
     }
 
     render() {
-        console.log(this.props, "props")
         return (
             <div>
                 <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
@@ -33,7 +54,9 @@ class Home extends React.Component {
                         <Form size='large'>
                             <Segment stacked>
                                 <Form.Input onChange={(e) => this.handleChange(e)} name="name" value={this.state.name} fluid icon='user' iconPosition='left' placeholder='User Name' />
-                                <Button color='teal' fluid size='large' onClick={() => this.signIn()}>
+                                <Form.Input onChange={(e) => this.handleChange(e)} name="password" value={this.state.password} icon='keyboard outline' fluid iconPosition='left' placeholder='Password' />
+                                <Form.Input onChange={(e) => this.handleChange(e)} name="password_confirmation" value={this.state.password_confirmation} fluid icon='lock' iconPosition='left' placeholder='Confirm Password' />
+                                <Button color='teal' fluid size='large' onClick={(e) => this.onSubmit(e)}>
                                     Login
                                 </Button>
                             </Segment>
